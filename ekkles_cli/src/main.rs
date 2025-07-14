284cb6cf0b8a72dc8e8fedffbfb0a1f0cdf9749f
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2025-07-14 14:43:20 +0200

    Zprovozněn program, přidání vlaječky pro povolení přepisu

--- ekkles_cli/src/main.rs
+++ ekkles_cli/src/main.rs
@@ -2,9 +2,10 @@ use anyhow::{Context, Result, bail};
 use clap::{Parser, ValueEnum};
 use ekkles_data::Song;
 use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-/// Malá utilitka k programu Ekkles, která slouží k importu písní (ve formátu Opensongu) a biblí (ve formátu z github repozitáře) do databáze Ekklesu.
+/// Malá utilitka k programu Ekkles, která slouží k importu písní (ve formátu Opensongu)
+/// a biblí (ve formátu z github repozitáře) do databáze Ekklesu.
 #[derive(Parser, Debug)]
 struct Cli {
     /// Co se bude parsovat
@@ -13,6 +14,12 @@ struct Cli {
     db_file: PathBuf,
     /// Vstupní XML soubory bible nebo písní
     input_files: Vec<PathBuf>,
+    /// Určuje, jak nakládat s biblemi/písněmi, které již v databázi existují.
+    /// Ve výchozím nastavení jsou takové vstupy ignorovány (v databázi jsou zachována
+    /// původní data), pokud je specifikována tato vlaječka, budou namísto toho
+    /// existující záznamy přepsány.
+    #[arg(long, short)]
+    overwrite_records: bool,
 }
 
 #[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq)]
@@ -23,19 +30,11 @@ enum ParseKind {
     Song,
 }
 
-async fn connect_to_db(path: &Path) -> Result<SqlitePool> {
-    let options = SqliteConnectOptions::new()
-        .filename(path)
-        .create_if_missing(true)
-        .optimize_on_close(true, None);
-
-    let pool = SqlitePool::connect_with(options)
-        .await
-        .context("Nelze se připojit k databázi")?;
-
-    Ok(pool)
-}
-
+/// Hlavní funkce programu, cyklus postupně načítá všechny soubory specifikované
+/// na příkazové řádce (`config`), každý se pokusí zparsovat a uložit do databáze.
+///
+/// ### Přepis existujícího záznamu
+/// Jestli se přepisuje záleží na konfiguraci (viz [`Cli`]).
 async fn run(config: Cli) -> Result<()> {
     let db_options = SqliteConnectOptions::new()
         .filename(config.db_file)
@@ -48,7 +47,7 @@ async fn run(config: Cli) -> Result<()> {
     let total = config.input_files.len();
     let mut successes = 0;
     let mut fails = 0;
-
+    println!("Úspěch + Selhání / Celkem");
     for input_file in config.input_files {
         match config.parse_kind {
             ParseKind::Bible => todo!(),
@@ -56,10 +55,17 @@ async fn run(config: Cli) -> Result<()> {
                 let res = Song::parse_from_xml_file(&input_file);
                 match res {
                     Ok(song) => {
-                        song.save_to_db(&db_pool).await.with_context(|| {
-                            format!("Nelze uložit píseň {} do databáze", song.title)
-                        })?;
-                        successes += 1;
+                        if config.overwrite_records {
+                            todo!()
+                        } else {
+                            match song.save_to_db(&db_pool).await {
+                                Ok(_) => successes += 1,
+                                Err(err) => {
+                                    eprintln!("{:?}", err);
+                                    fails += 1;
+                                }
+                            };
+                        }
                     }
                     Err(err) => {
                         eprintln!(
@@ -70,7 +76,7 @@ async fn run(config: Cli) -> Result<()> {
                         fails += 1;
                     }
                 }
-                println!("{:04} + {:04} / {:04}", successes, fails, total);
+                println!("{:04}   + {:04}    / {:04}", successes, fails, total);
             }
         }
     }
@@ -81,14 +87,14 @@ async fn run(config: Cli) -> Result<()> {
     Ok(())
 }
 
-fn main() -> Result<()> {
+// Spustí jednovláknový runtime, na prostý import písní nepotřebujeme spouštět vícevláknovou aplikaci
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
     let config = Cli::parse();
 
     if config.input_files.is_empty() {
         bail!("Nebyly zadány žádné vstupní soubory k parsování, končím");
     }
 
-    eprintln!("Načtena konfigurace: {:#?}", config);
-
-    Ok(())
+    run(config).await
 }
